259a2b4d7ea9db281b8f1a49a8b81d4b407bebc3
Lucas <lucassoares2007@hotmail.com>
2022-11-22 23:59:29 -0300

    build: sprint1/2

--- src/app.js
+++ src/app.js
@@ -1,21 +1,30 @@
-const express = require("express");
+import express from "express";
+import users from "./database";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 app.use(express.json());
 
+app.post("/users", (request, response) => {
+  const user = {
+    uuid: uuidv4(),
+    ...request.body,
+  };
+  users.push(user);
+  return response.status(201).json(user);
+});
+
 app.get("/users", (request, response) => {
-  console.log(request.body);
-  const users = [
-    {
-      name: "Cauan",
-      age: 22,
-    },
-    {
-      name: "Maykel",
-      age: 25,
-    },
-  ];
   return response.json(users);
+});
+
+app.get("/users/:id", (request, response) => {
+  const id = request.params.id;
+  const user = users.find((el) => el.uuid === id);
+  if (!user) {
+    return response.status(404).json({ message: "User not found" });
+  }
+  return response.json(user);
 });
 
 app.listen(3000, () => {
