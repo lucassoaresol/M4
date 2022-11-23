e3968bbf4f7dc6b38128d48190448b93321c4414
Lucas <lucassoares2007@hotmail.com>
2022-11-23 16:51:19 -0300

    build: sprint1/3

--- src/app.js
+++ src/app.js
@@ -5,27 +5,66 @@ import { v4 as uuidv4 } from "uuid";
 const app = express();
 app.use(express.json());
 
-app.post("/users", (request, response) => {
+const createUserService = (userData) => {
   const user = {
     uuid: uuidv4(),
-    ...request.body,
+    ...userData,
+    createdAt: new Date(),
   };
   users.push(user);
-  return response.status(201).json(user);
-});
+  return [201, user];
+};
 
-app.get("/users", (request, response) => {
-  return response.json(users);
-});
+const listUserService = (module) => {
+  if (module) {
+    const filterUsers = users.filter((el) => el.module === module);
+    return filterUsers;
+  }
+  return users;
+};
 
-app.get("/users/:id", (request, response) => {
-  const id = request.params.id;
+const retrieveUserService = (id) => {
   const user = users.find((el) => el.uuid === id);
   if (!user) {
-    return response.status(404).json({ message: "User not found" });
+    return [404, { message: "User not found!" }];
   }
-  return response.json(user);
-});
+  return [200, user];
+};
+
+const deleteUserService = (id) => {
+  const user = users.find((el) => el.uuid === id);
+  if (!user) {
+    return [404, { message: "User not found!" }];
+  }
+  const index = users.findIndex((el) => el.uuid === id);
+  users.splice(index, 1);
+  return [204, {}];
+};
+
+const createUserController = (request, response) => {
+  const [status, data] = createUserService(request.body);
+  return response.status(status).json(data);
+};
+
+const listUserController = (request, response) => {
+  const usersData = listUserService(request.query.module);
+  return response.json(usersData);
+};
+
+const retrieveUserController = (request, response) => {
+  const [status, data] = retrieveUserService(request.params.id);
+  return response.status(status).json(data);
+};
+
+const deleteUserController = (request, response) => {
+  const [status, data] = deleteUserService(request.params.id);
+  return response.status(status).json(data);
+};
+
+app.post("/users", createUserController);
+app.get("/users", listUserController);
+app.get("/users/:id", retrieveUserController);
+app.delete("/users/:id", deleteUserController);
 
 app.listen(3000, () => {
   console.log("Server running in port 3000");
