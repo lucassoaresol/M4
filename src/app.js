77e16b82f38d890c810f9f166563b1f716f1b3cf
Lucas <lucassoares2007@hotmail.com>
2022-11-25 02:29:12 -0300

    build:sprint1/4

--- src/app.js
+++ src/app.js
@@ -1,14 +1,56 @@
-import express from "express";
-import users from "./database";
-import { v4 as uuidv4 } from "uuid";
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import { compare, hash } from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import users from './database';
+import 'dotenv/config';
 
 const app = express();
 app.use(express.json());
 
-const createUserService = (userData) => {
+const ensureAuthMiddleware = (request, response, next) => {
+  let { authorization } = request.headers;
+
+  if (!authorization) {
+    return response.status(401).json({
+      message: 'Invalid token',
+    });
+  }
+
+  authorization = authorization.split(' ')[1];
+
+  return jwt.verify(authorization, process.env.SECRET_KEY, (error, decoded) => {
+    if (error) {
+      return [
+        401,
+        {
+          message: 'Invalid token',
+        },
+      ];
+    }
+    request.user = {
+      id: decoded.sub,
+    };
+    return next();
+  });
+};
+
+const ensureUserExistsMiddleware = (request, response, next) => {
+  const userIndex = users.findIndex((el) => el.uuid === request.params.id);
+  if (userIndex === -1) {
+    return response.status(404).json({ message: 'User not found!' });
+  }
+  request.user = {
+    userIndex,
+  };
+  return next();
+};
+
+const createUserService = async (userData) => {
   const user = {
     uuid: uuidv4(),
     ...userData,
+    password: await hash(userData.password, 10),
     createdAt: new Date(),
   };
   users.push(user);
@@ -18,54 +60,89 @@ const createUserService = (userData) => {
 const listUserService = (module) => {
   if (module) {
     const filterUsers = users.filter((el) => el.module === module);
-    return filterUsers;
+    return [200, filterUsers];
   }
-  return users;
+  return [200, users];
 };
 
-const retrieveUserService = (id) => {
-  const user = users.find((el) => el.uuid === id);
-  if (!user) {
-    return [404, { message: "User not found!" }];
-  }
-  return [200, user];
-};
+const retrieveUserService = (index) => [200, users[index]];
 
-const deleteUserService = (id) => {
-  const user = users.find((el) => el.uuid === id);
-  if (!user) {
-    return [404, { message: "User not found!" }];
-  }
-  const index = users.findIndex((el) => el.uuid === id);
+const deleteUserService = (index) => {
   users.splice(index, 1);
   return [204, {}];
 };
 
-const createUserController = (request, response) => {
-  const [status, data] = createUserService(request.body);
+const createSessionService = async ({ email, password }) => {
+  const user = users.find((el) => el.email === email);
+  if (!user) {
+    return [
+      401,
+      {
+        message: 'Wrong email or password',
+      },
+    ];
+  }
+
+  const passwordMatch = await compare(password, user.password);
+  if (!passwordMatch) {
+    return [
+      401,
+      {
+        message: 'Wrong email or password',
+      },
+    ];
+  }
+
+  const token = jwt.sign(
+    {
+      age: user.age,
+    },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: '24h',
+      subject: user.uuid,
+    }
+  );
+
+  return [200, { token }];
+};
+
+const createUserController = async (request, response) => {
+  const [status, data] = await createUserService(request.body);
   return response.status(status).json(data);
 };
 
 const listUserController = (request, response) => {
-  const usersData = listUserService(request.query.module);
-  return response.json(usersData);
+  const [status, data] = listUserService(request.query.module);
+  return response.status(status).json(data);
 };
 
 const retrieveUserController = (request, response) => {
-  const [status, data] = retrieveUserService(request.params.id);
+  const [status, data] = retrieveUserService(request.user.userIndex);
   return response.status(status).json(data);
 };
 
 const deleteUserController = (request, response) => {
-  const [status, data] = deleteUserService(request.params.id);
+  const [status, data] = deleteUserService(request.user.userIndex);
   return response.status(status).json(data);
 };
 
-app.post("/users", createUserController);
-app.get("/users", listUserController);
-app.get("/users/:id", retrieveUserController);
-app.delete("/users/:id", deleteUserController);
+const createSessionController = async (request, response) => {
+  const [status, data] = await createSessionService(request.body);
+  return response.status(status).json(data);
+};
+
+app.post('/users', createUserController);
+app.get('/users', ensureAuthMiddleware, listUserController);
+app.get(
+  '/users/:id',
+  ensureAuthMiddleware,
+  ensureUserExistsMiddleware,
+  retrieveUserController
+);
+app.delete('/users/:id', ensureAuthMiddleware, deleteUserController);
+app.post('/login', createSessionController);
 
 app.listen(3000, () => {
-  console.log("Server running in port 3000");
+  console.log('Server running in port 3000');
 });
